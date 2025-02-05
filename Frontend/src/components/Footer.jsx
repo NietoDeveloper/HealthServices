8d2352af1968e381eb8d3f87f5e248f54d43a4bf
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-02-04 21:21:09 -0500

    Ready

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   const socialLinks = [
-    { icon: FaTwitter, href: 'https://x.com/YazdanH7', color: 'text-blue-400' },
-    { icon: FaFacebookF, href: 'https://www.facebook.com/yazdan.yazdankhan.7/', color: 'text-blue-600' },
-    { icon: FaInstagram, href: 'https://www.instagram.com/yazdan.haider23/', color: 'text-pink-500' },
-    { icon: FaLinkedinIn, href: 'https://www.linkedin.com/in/yazdan-haider', color: 'text-blue-700' },
+    { icon: FaTwitter, href: '' },
+    { icon: FaFacebookF, href: '', color: 'text-blue-600' },
+    { icon: FaInstagram, href: '', color: 'text-pink-500' },
+    { icon: FaLinkedinIn, href: '', color: 'text-blue-700' },
   ];
 
   const footerLinks = [
@@ -62,10 +62,10 @@ const Footer = () => {
         </div>
         <div className="mt-8 pt-6 border-t border-gray-700 text-center text-sm">
           <p>
-            © {currentYear} Health Nest. All rights reserved.
+            © {currentYear} All rights reserved.
           </p>
           <p className="mt-2 flex items-center justify-center">
-            Made with <FaHeart className="text-accent mx-1" /> by Health Nest Team
+            Nieto Software Developer
           </p>
         </div>
       </div>
