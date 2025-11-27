309c95b937279c0e254db3f27e8c15dc7902f86c
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-27 10:44:53 -0500

    Update Header.jsx

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
                 <div className="flex justify-between items-center">
                     <Link to="/" className="flex items-center space-x-2">
                         <img src={Logo} alt="Health Nest Logo" className="h-10 w-auto" />
-                        <span className="text-2xl font-bold text-accent">Personal Health Center</span>
+                        <span className="text-2xl font-bold text-accent">Health Center</span>
                     </Link>
 
                     <nav className="hidden md:flex space-x-1">
