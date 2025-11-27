47514c962241677c8714f9b7c3e169584d6138c2
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-27 11:16:19 -0500

    Update HomePage.jsx

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -4,8 +4,8 @@ import Layout from '../components/Layout';
 const HomePage = () => {
   return (
     <Layout>
-      <h1 className="text-3xl font-bold text-center my-8">Welcome to Hospital Management System</h1>
-      {/* Add more sections or components as needed */}
+      <h1 className="text-3xl font-bold text-center my-8">Welcome to Health Center Management System</h1>
+
     </Layout>
   );
 };
