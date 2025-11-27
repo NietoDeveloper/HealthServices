e5903aa8f03d717d63558705230b493cc03faa87
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-27 11:59:27 -0500

    Update PatientRoute.js

--- Backend/api/routes/PatientRoute.js
+++ Backend/api/routes/PatientRoute.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.post('/register', registerPatient);
 
-// Route for logging in a patient
 router.post('/login', loginPatient);
 
 router.get("/get-patients", getAllPatients);
