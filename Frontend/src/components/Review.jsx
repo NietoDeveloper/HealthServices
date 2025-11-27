4dd0f793ad536421af2eb94a9038cc562046546b
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-27 11:55:14 -0500

    Update Review.jsx

--- Frontend/src/components/Review.jsx
+++ Frontend/src/components/Review.jsx
@@ -54,7 +54,7 @@ const Review = () => {
 
             <form onSubmit={handleSubmit}>
   <textarea
-    className="w-full p-2 rounded-lg bg-navy-700 text-black focus:outline-none mb-4" // changed text-white to text-black
+    className="w-full p-2 rounded-lg bg-navy-700 text-black focus:outline-none mb-4" 
     placeholder="Leave a comment..."
     rows="4"
     value={comment}
